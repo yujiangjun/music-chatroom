0b0ca78b8a3ae5fd1eb666b982ef00ea72d711d8
余江俊 <18512106361@163.com>
2022-01-15 13:38:50 +0800

    完成websocket保活

---
+++ src/api/base.js
@@ -0,0 +1,3 @@
+let wsUrl='ws://127.0.0.1:8888/'
+
+export default wsUrl;
--- src/view/chat.vue
+++ src/view/chat.vue
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import wsUrl from "@/api/base";
 const data = [
   {
     title: '消息1',
@@ -48,15 +49,92 @@ export default {
   data(){
     return{
       data,
-      msg:''
+      msg:'',
+      socket: null,
+      heartCheck:{
+        timeout:5000,
+        timeoutObj:null,
+        serverTimeoutObj:null,
+        reset:this.heartCheckRest,
+        start:this.heartCheckStart
+      }
     }
   },
+  mounted() {
+    this.socketInit()
+    window.onbeforeunload=()=>this.socket.close();
+  },
   methods:{
+    socketInit(){
+      this.socket=new WebSocket(wsUrl)
+      this.socket.onpen=this.socketOpen
+      this.socket.onerror=this.socketError
+      this.socket.onmessage=this.socketGetMsg
+      this.socket.onclose=this.socketClose
+    },
     sendMsg(){
-      this.data.push({
-        title: this.msg+this.data.length
-      })
+      this.socketSend()
       this.msg=''
+    },
+    socketOpen(){
+      console.log('链接成功')
+      this.heartCheck.reset()
+      this.heartCheck.start()
+    },
+    socketError(e){
+      console.error('发生错误')
+      console.error(e)
+      this.socketReconnect()
+    },
+    socketSend(){
+      this.socket.send(this.msg)
+    },
+    socketGetMsg(msg){
+      console.log(msg)
+      if (msg!='hb_request' && msg.data!='heartBeat')
+      this.data.push({
+        title: msg.data
+      })
+      this.heartCheck.reset()
+      this.heartCheck.start()
+    },
+    socketClose(){
+      console.warn('链接关闭')
+      this.heartCheck.reset()
+      this.socketReconnect()
+    },
+    heartCheckRest(){
+      this.heartCheck.timeoutObj=null
+      this.heartCheck.serverTimeoutObj=null
+    },
+    socketReconnect(){
+      var lockReconnect = false, tt;
+      if (lockReconnect) {
+        return;
+      }
+      lockReconnect = true;
+      tt && clearTimeout(tt);
+      let that=this
+      tt = setTimeout(function () {
+        console.log('重连中...');
+        lockReconnect = false;
+        that.socketInit();
+      }, 4000);
+    },
+    heartCheckStart(){
+      let self=this.heartCheck
+      this.heartCheck.timeout&&clearTimeout(this.heartCheck.timeoutObj)
+      this.heartCheck.serverTimeoutObj&&clearTimeout(this.heartCheck.serverTimeoutObj)
+      this.heartCheck.timeoutObj=setTimeout(()=>{
+        console.log('当前ws状态:'+this.socket.readyState)
+        if (this.socket.readyState !=1){
+          return
+        }
+        this.socket.send("heartBeat")
+        self.serverTimeoutObj=setTimeout(()=>{
+          this.socket.close()
+        },self.timeout)
+      },this.heartCheck.timeout)
     }
   }
 }
