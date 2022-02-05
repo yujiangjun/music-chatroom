8eae78540d0dbd7087d7d17bb9f06c2fa0f58b22
余江俊 <18512106361@163.com>
2022-02-05 09:59:41 +0800

    集成gitee

--- src/view/chat.vue
+++ src/view/chat.vue
@@ -2,7 +2,13 @@
 <div>
   <a-row style="height: 100%">
     <a-col span="12">
-      <BaseChat/>
+<!--      <BaseChat/>-->
+      <a-tabs v-model="currentRoomId" type="editable-card" hideAdd>
+        <a-tab-pane v-for="room in openRooms" :key="room.id" :tab="room.roomName" >
+<!--          {{ pane.roomName }}-->
+          <BaseChat/>
+        </a-tab-pane>
+      </a-tabs>
     </a-col>
     <a-col span="5" offset="1">
       <player :play-url="playUrl" :song-mid="songMid" :albummid="albummid" v-if="playUrl" style="width: 100%"/>
@@ -13,7 +19,7 @@
         <div v-for="item in roomList" :key="item.id">
           <img :src="item.roomImg" style="width: 40px;border-radius: 20px"/>
           <b style="margin-left: 15px">{{item.roomName}}</b>
-          <a-button type="primary" style="margin-left: 15px" icon="enter" size="small">进入</a-button>
+          <a-button type="primary" style="margin-left: 15px" icon="enter" size="small" @click="enterRoom(item)">进入</a-button>
         </div>
       </div>
     </a-col>
@@ -40,7 +46,9 @@ export default {
       albummid:'',
       songMid:'',
       uploadUrl: baseRestUrl+'/oss/upload',
-      roomList:[]
+      roomList:[],
+      currentRoomId:0,
+      openRooms:[]
       // heartCheck:{
       //   timeout:5000,
       //   timeoutObj:null,
@@ -64,6 +72,20 @@ export default {
         this.roomList=res.data
       })
     },
+    enterRoom(room){
+      let isEnter=false;
+      this.openRooms.forEach(item=>{
+        if (room.id==item.id){
+          isEnter=true
+          return
+        }
+      })
+      if (isEnter){
+        return
+      }
+      this.currentRoomId+=1
+      this.openRooms.push(room)
+    }
   }
 }
 </script>
